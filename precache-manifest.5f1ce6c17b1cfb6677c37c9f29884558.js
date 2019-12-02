1291ea77414f5e665147bfbc0373ac005364a343
jain_stacks <pardeep.jain@9stacks.com>
2019-12-02 14:44:40 +0530

    Init commit

---
+++ precache-manifest.5f1ce6c17b1cfb6677c37c9f29884558.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61c7b61dfbaedaafd26a1962478bab3e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a8c616ba4251f1ff47ba",
+    "url": "/static/css/main.8a593890.chunk.css"
+  },
+  {
+    "revision": "c1e946ad9890337215a9",
+    "url": "/static/js/2.4742f7d7.chunk.js"
+  },
+  {
+    "revision": "a8c616ba4251f1ff47ba",
+    "url": "/static/js/main.76929e89.chunk.js"
+  },
+  {
+    "revision": "c145cd62cd32d6ab606b",
+    "url": "/static/js/runtime-main.340ff8a8.js"
+  }
+]);
